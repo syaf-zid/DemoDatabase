9794838cfcd785a924603966973b52f115d4ca4f
syafzid <18052270@myrp.edu.sg>
2020-05-15 11:16:44 +0800

    Completed with Section C Worksheet

--- app/src/main/java/com/example/demodatabase/DBHelper.java
+++ app/src/main/java/com/example/demodatabase/DBHelper.java
@@ -1,5 +1,6 @@
 package com.example.demodatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,7 +16,7 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String TABLE_TASK = "task";
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_DESCRIPTION = "description";
-    private static final String COLUMN_NAME = "date";
+    private static final String COLUMN_DATE = "date";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VER);
@@ -25,7 +26,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String createTableSQL = "CREATE TABLE " + TABLE_TASK + "("
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_NAME + " TEXT,"
+                + COLUMN_DATE + " TEXT,"
                 + COLUMN_DESCRIPTION + " TEXT)";
         sqLiteDatabase.execSQL(createTableSQL);
         Log.i("info", "created tables");
@@ -37,5 +38,20 @@ public class DBHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
         // Create table(s) again
         onCreate(sqLiteDatabase);
+    }
+
+    public void insertTask(String description, String date) {
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the description as value
+        values.put(COLUMN_DESCRIPTION, description);
+        // Store the column name as key and the date as value
+        values.put(COLUMN_DATE, date);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_TASK, null, values);
+        // Close the database connection
+        db.close();
     }
 }
--- app/src/main/java/com/example/demodatabase/MainActivity.java
+++ app/src/main/java/com/example/demodatabase/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.demodatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.buttonInsert);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create DBHelper object, passing in the activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
     }
 }
